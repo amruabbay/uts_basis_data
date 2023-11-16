10442fa54857af991586e497d20285c24bec98fc
amruabbay <151052604+amruabbay@users.noreply.github.com>
2023-11-17 00:16:07 +0700

    Delete Jawaban3.sql

--- Jawaban3.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `Jawaban3` (`id_mahasiswa`, `nama`, `nim`, `prodi`, `tanggal_lahir`, `alamat`, `no_hp`, `email`) VALUES (1, 'Huang Renjun', '12345', 'Teknik Informatika', '2000-01-01', 'Jl. Sunda No. 123', '08125367890', 'yellow@gmail.com');
-INSERT INTO `Jawaban3` (`id_mahasiswa`, `nama`, `nim`, `prodi`, `tanggal_lahir`, `alamat`, `no_hp`, `email`) VALUES (2, 'Moonbin', '67890', 'Sistem Informasi', '1999-05-15', 'Jl. Sample No. 456', '0874557689', 'star@gmail.com');
-INSERT INTO `Jawaban3` (`id_dosen`, `nama_dosen`, `nip`, `bidang`, `email`, `no_hp`) VALUES (1, 'Dr. Ahmad', '123456', 'Sistem Komputer', 'ahmad@example.com', '08123456789');
-INSERT INTO `Jawaban3` (`id_dosen`, `nama_dosen`, `nip`, `bidang`, `email`, `no_hp`) VALUES (2, 'Prof. Budi', '789012', 'Jaringan Komputer', 'budi@example.com', '08765432100');
-INSERT INTO `Jawaban3` (`id_mata_kuliah`, `nama_mata_kuliah`, `sks`, `deskripsi`) VALUES (1, 'Pemrograman Web', 3, 'Belajar membuat aplikasi web');
-INSERT INTO `Jawaban3` (`id_mata_kuliah`, `nama_mata_kuliah`, `sks`, `deskripsi`) VALUES (2, 'Basis Data', 4, 'Konsep dan implementasi basis data');
-INSERT INTO `Jawaban3` (`id_mata_kuliah`, `nama_mata_kuliah`, `sks`, `deskripsi`) VALUES (3, 'Jaringan Komputer', 3, 'Dasar-dasar jaringan komputer');
