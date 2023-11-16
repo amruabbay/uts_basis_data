3399f766b23bd82d3833b1fa3fc064257c36393f
amruabbay <151052604+amruabbay@users.noreply.github.com>
2023-11-16 23:38:55 +0700

    Add files via upload

---
+++ Jawaban5.sql
@@ -0,0 +1,2 @@
+INSERT INTO `Jawaban5` (`id_dosen`, `nama_dosen`, `id_mata_kuliah`, `nama_mata_kuliah`, `sks`, `deskripsi`) VALUES (1, 'Dr. Ahmad', 1, 'Pemrograman Web', 3, 'Belajar membuat aplikasi web');
+INSERT INTO `Jawaban5` (`id_dosen`, `nama_dosen`, `id_mata_kuliah`, `nama_mata_kuliah`, `sks`, `deskripsi`) VALUES (2, 'Prof. Budi', 1, 'Pemrograman Web', 3, 'Belajar membuat aplikasi web');
---
+++ Jawaban6.sql
@@ -0,0 +1,2 @@
+INSERT INTO `Jawaban6` (`id_dosen`, `nama_dosen`, `id_mata_kuliah`, `nama_mata_kuliah`, `id_mahasiswa`, `nama`) VALUES (1, 'Dr. Ahmad', 1, 'Pemrograman Web', 1, 'Huang Renjun');
+INSERT INTO `Jawaban6` (`id_dosen`, `nama_dosen`, `id_mata_kuliah`, `nama_mata_kuliah`, `id_mahasiswa`, `nama`) VALUES (2, 'Prof. Budi', 1, 'Pemrograman Web', 1, 'Huang Renjun');
